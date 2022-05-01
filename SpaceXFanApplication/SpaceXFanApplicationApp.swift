0662a0c0e8f70c67de96e20d7928944e3850211f
ahmetcanpoyraz <poyrazahmetcan@hotmail.com>
2022-05-01 20:18:36 +0300

    Initial Commit

---
+++ SpaceXFanApplication/SpaceXFanApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXFanApplicationApp.swift
+//  SpaceXFanApplication
+//
+//  Created by Ahmet can Poyraz on 1.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXFanApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
